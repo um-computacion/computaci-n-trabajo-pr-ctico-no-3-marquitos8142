721a587ef51744218b925ccb4f101c79961824f7
marquitos8142 <nicolasbeltran880@gmail.com>
2025-04-15 15:41:31 -0300

    Implementacion de la funcion ingrese_numero

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,4 +2,12 @@ class NumeroDebeSerPositivo(Exception):
     pass
 
 def ingrese_numero():
-    pass
+
+    entrada = input("Ingrese un número: ")
+    try:
+        numero = int(entrada)
+        if numero < 0:
+            raise NumeroDebeSerPositivo("El número debe ser positivo")
+        return numero
+    except ValueError:
+        raise ValueError("La entrada debe ser un número válido") 
